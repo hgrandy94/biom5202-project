7055b11ad2c50acbf3a58121ce5e444e1b9c2b7d
Heather Grandy <heather.grandy@hotmail.com>
2023-11-30 16:27:46 -0500

    Added canny edge detection

--- pipeline.py
+++ pipeline.py
@@ -2,9 +2,12 @@
 import numpy as np
 from preprocessing_functions import apply_median_filter
 from preprocessing_functions import apply_gaussian_filter
+from preprocessing_functions import canny_edge
 from segmentation_functions import kmeans_segmentation
 
 def pipeline(img):
     # img = apply_gaussian_filter(img, sigma=2, kernel_size=3)
-    img = apply_median_filter(img, kernel_size=3)
+    # img = apply_median_filter(img, kernel_size=3)
+    # img = canny_edge(img, sigma=3)
+    img = apply_median_filter(img, kernel_size=9)
     return img
--- preprocessing_functions.py
+++ preprocessing_functions.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage
-import skimage
+from skimage import feature
 
 # Define functions to perform general preprocessing steps
 # NOTE: Approach and Default values obtained form yashbhalgat MRNet-Competition on GitHub
@@ -105,41 +105,7 @@ def apply_median_filter(img, kernel_size):
     
     return filtered_img
 
-def apply_laplace_filter(input_img, k, kernel_size=9):
-    """
-    This function applies a Laplacian filter to a data array and was designed
-    with the intent of filtering image arrays. It clips the laplace values to adjust
-    results to a valid scale, and finally plots the Laplacian-filtered image.
-
-    Parameters
-    ------------
-    input_img: 2D numpy array
-        Array to which the Laplacian filter will be applied (image array)
-    k: integer
-        Scaling factor to adjust weight of edges.
-    kernel_size: integer
-        Kernel size for the Laplacian transform. Default=9 if not specified
-
-    Output
-    ------------
-    laplace_clipped: 2D numpy array
-        Image array with Laplacian transform and clipping applied.
-    """
-    # Setup Laplace filter using skimage library
-    laplace_filter = skimage.filters.laplace(input_img, ksize=kernel_size)
-
-    # Apply the Laplace filter to the input image
-    apply_laplace = input_img + k * laplace_filter
-
-    # Adjust clipping to valid scale
-    # laplace_clipped =apply_laplace
-    laplace_clipped = np.clip(apply_laplace, 0, 1)
-
-    # Plot the Laplacian-enhanced knee x-ray image
-    #fig, ax = plt.subplots(1,1)
-    ##ax[0].imshow(knee_array_scaled, cmap="Greys_r")
-    #ax.imshow(laplace_clipped, cmap="Greys_r")
-    #ax[0].set_title("Original Image")
-    #ax.set_title(f"Image with Laplacian Filter, k={k}")
-    #plt.tight_layout();
-    return laplace_clipped
+def canny_edge(img, sigma=3):
+    edge_canny = feature.canny(img.astype('float32'), sigma=sigma) # this outputs a boolean array
+    edge_canny = edge_canny.astype(np.uint8) * 255 # convert to uint8
+    return edge_canny
